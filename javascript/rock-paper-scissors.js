bfe4fbcd694ec4ada705018187473ffdc8838320
Gustavo <gustavobernardo094@gmail.com>
2022-01-28 10:32:54 -0300

    - Creating function: Rounds and game the function game should loop the function Rounds 5 times, declare const to all the messages that game and rounds should return.

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -2,17 +2,61 @@
 // The computer has to randomly choose between rock, paper or scissors
 // And the player choose, after the two sides pick their option, compare who won and show a message
 
+/* 
+1 - computer select 
+2 - user select
+3 - if statement to compare and see who won
+  3.1 -  if user = rock and computer = paper: Computer won
+  3.2 -  if user = rock and computer = scissor: user won
+  3.3 -  if user = paper and computer = scissor: computer won
+
+4 - display who won
+5 - repeat the process in total five times
+
+
+
+
+
+
+*/
+
 
 const rockPaperScissor = ['Rock', 'Paper', 'Scissor'];
-console.log(rockPaperScissor);
+// console.log(rockPaperScissor);
 let numRandom = Math.random(1);
 let x = Math.floor(numRandom * rockPaperScissor.length);
 
+function computerPlay(){
+  return rockPaperScissor[x]
+}
+// console.log(computerPlay());
 
-// let numFloor = Math.floor(numRandom);
-console.log(x);
+function rounds(playerSelection, computerSelection){
+  const loseToPaper = 'You lose! Paper beats Rock';
+  const wonPaper = 'You won! Paper beats Rock';
+  const loseToRock = 'You lose! Rock beats Scissor';
+  const wonRock = 'You won! Rock beats Scissor';
+  const loseToScissor = 'You lose! Scissor beats Paper';
+  const wonScissor = 'You won! Scissor beats Paper';
+  if (playerSelection === 'rock' && computerPlay === 'paper'){
+      return console.log(loseToPaper)
+  }
+  // return loseToPaper;
+}
 
+playerSelection = 'Rock' /* prompt('Choose: Rock, Paper or Scissor'); */
+computerSelection = computerPlay();
 
+// console.log(rounds(playerSelection, computerPlay));
+
+function game(){
+  for (let i = 0; i <= 5 ; i++){
+
+    rounds();
+    console.log(i);
+  }
+}
+// game();
 
 // function computerPlay(){
 //   return console.log(rockPaperScissor.mathrandom());
