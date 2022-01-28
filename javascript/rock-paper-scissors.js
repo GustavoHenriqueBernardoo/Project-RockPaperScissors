ed65069d5f97e6cd3f188332776df7b87abc8d9f
Gustavo <gustavobernardo094@gmail.com>
2022-01-28 11:39:40 -0300

    Start building the if statements and playing around with the switch as well, perhaps I will let the two together just for the sake of practicing.

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -12,12 +12,6 @@
 
 4 - display who won
 5 - repeat the process in total five times
-
-
-
-
-
-
 */
 
 
@@ -38,16 +32,41 @@ function rounds(playerSelection, computerSelection){
   const wonRock = 'You won! Rock beats Scissor';
   const loseToScissor = 'You lose! Scissor beats Paper';
   const wonScissor = 'You won! Scissor beats Paper';
-  if (playerSelection === 'rock' && computerPlay === 'paper'){
-      return console.log(loseToPaper)
+  switch(playerSelection, computerSelection){
+    case playerSelection === 'Rock' && computerSelection === 'Rock':
+      console.log('Tie!!!')
+      break;
+    case playerSelection === 'Scissor' && computerSelection === 'Scissor':
+      console.log('Tie!!!')
+      break;
+    case playerSelection === 'Paper' && computerSelection === 'Paper':
+      console.log('Tie!!!')
+      break;
+    default:
+      console.log('WTF')
+      break;
+  }
+  if (playerSelection === 'Rock' && computerSelection === 'Paper'){
+      return loseToPaper;
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+    return wonPaper;
+  } else if (playerSelection === 'Scissor' && computerSelection === 'Rock'){
+  return loseToRock;
+  } else if (playerSelection === 'Rock' && computerSelection === 'Scissor'){
+    return wonRock;
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissor'){
+    return loseToScissor;
+  } else if (playerSelection === 'Scissor' && computerSelection === 'Paper'){
+    return wonScissor;
   }
   // return loseToPaper;
+
 }
 
-playerSelection = 'Rock' /* prompt('Choose: Rock, Paper or Scissor'); */
-computerSelection = computerPlay();
+const playerSelection = 'Rock' /* prompt('Choose: Rock, Paper or Scissor'); */
+const computerSelection = computerPlay();
 
-// console.log(rounds(playerSelection, computerPlay));
+console.log(rounds(playerSelection, computerSelection));
 
 function game(){
   for (let i = 0; i <= 5 ; i++){
