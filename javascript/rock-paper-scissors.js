42eb0ba759dd9eb722eff100b1e6a45b565bc769
Gustavo <gustavobernardo094@gmail.com>
2022-01-29 08:18:58 -0300

    Trying to prevent the user from enter another value

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -7,15 +7,20 @@
 2 - user select
 3 - if statement to compare and see who won
   3.1 -  if user = rock and computer = paper: Computer won
+    put +1 score to CPU
   3.2 -  if user = rock and computer = scissor: user won
+    put +1 score to user
   3.3 -  if user = paper and computer = scissor: computer won
+    put +1 score to CPU
 
 4 - display who won
 5 - repeat the process in total five times
 */
 
 
-const rockPaperScissor = ['Rock', 'Paper', 'Scissor'];
+const rockPaperScissor = ['ROCK', 'PAPER', 'SCISSOR'];
+let cpu = 0;
+let user = 0;
 // console.log(rockPaperScissor);
 let numRandom = Math.random(1);
 let x = Math.floor(numRandom * rockPaperScissor.length);
@@ -32,46 +37,66 @@ function rounds(playerSelection, computerSelection){
   const wonRock = 'You won! Rock beats Scissor';
   const loseToScissor = 'You lose! Scissor beats Paper';
   const wonScissor = 'You won! Scissor beats Paper';
-  if(playerSelection === 'Rock' && computerSelection === 'Rock'){
+  if(playerSelection === 'ROCK' && computerSelection === 'ROCK'){
       console.log('Tie!!!')
-  }else if (playerSelection === 'Scissor' && computerSelection === 'Scissor'){
+  }else if (playerSelection === 'SCISSOR' && computerSelection === 'SCISSOR'){
     console.log('Tie!!!')
-  }else if (playerSelection === 'Paper' && computerSelection === 'Paper'){
+  }else if (playerSelection === 'PAPER' && computerSelection === 'PAPER'){
       console.log('Tie!!!')
   }
 
 
 
-  if (playerSelection === 'Rock' && computerSelection === 'Paper'){
-      return loseToPaper;
-  } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+  if (playerSelection === 'ROCK' && computerSelection === 'PAPER'){
+    cpu++;
+    return loseToPaper;
+  } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+    user++;
     return wonPaper;
-  } else if (playerSelection === 'Scissor' && computerSelection === 'Rock'){
-  return loseToRock;
-  } else if (playerSelection === 'Rock' && computerSelection === 'Scissor'){
+  } else if (playerSelection === 'SCISSOR' && computerSelection === 'ROCK'){
+    cpu++;
+    return loseToRock;
+  } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSOR'){
+    user++;
     return wonRock;
-  } else if (playerSelection === 'Paper' && computerSelection === 'Scissor'){
+  } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSOR'){
+    cpu++;
     return loseToScissor;
-  } else if (playerSelection === 'Scissor' && computerSelection === 'Paper'){
+  } else if (playerSelection === 'SCISSOR' && computerSelection === 'PAPER'){
+    user++;
     return wonScissor;
-  }
+  } 
+  // else {
+  //   console.log('You type it something else, please try again.') 
+  // }
   // return loseToPaper;
 
 }
 
-const playerSelection = 'Rock' /* prompt('Choose: Rock, Paper or Scissor'); */
-const computerSelection = computerPlay();
+// const playerSelection = prompt('Choose: Rock, Paper or Scissor'); 
 
-console.log(rounds(playerSelection, computerSelection));
+// const computerSelection = computerPlay();
+
+// console.log(rounds(playerSelection.toUpperCase(), computerSelection));
+// console.log('CPU score:'+ cpu, 'User score:'+ user)
 
 function game(){
-  for (let i = 0; i <= 5 ; i++){
-
+  for (let i = 1; i <= 5 ; i++){
+    const playerSelection = prompt('Choose: Rock, Paper or Scissor'); 
+    playerSelectionUpper = playerSelection.toUpperCase();
+    // if(playerSelectionUpper != rockPaperScissor[0] || playerSelectionUpper !== rockPaperScissor[1] || playerSelectionUpper !== rockPaperScissor[2]){
+    //   console.log('WTF');
+    //   continue;
+    // }
+    const computerSelection = computerPlay();
+    console.log('Round:'+ i);
     rounds();
-    console.log(i);
+    console.log(rounds(playerSelectionUpper, computerSelection));
+    console.log('CPU score:'+ cpu, 'User score:'+ user)
+    
   }
 }
-// game();
+game();
 
 // function computerPlay(){
 //   return console.log(rockPaperScissor.mathrandom());
